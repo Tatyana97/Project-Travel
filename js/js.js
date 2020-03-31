e5345ed6328980ab4da6dd0b6000af3d5f3a3d15
Tatyana <tanyusha.miranovich.97@mail.ru>
2020-04-01 00:10:58 +0200

    create time event

--- js/js.js
+++ js/js.js
@@ -1,37 +1,78 @@
-window.addEventListener("DOMContentLoaded", function() {
-'use strict'
+window.addEventListener("DOMContentLoaded", function () {
+    'use strict'
 
     let tab = document.querySelectorAll(".info-header-tab"),
         info = document.querySelector(".info-header"),
         tabContent = document.querySelectorAll(".info-tabcontent");
 
-        function hideTabContent(a) {
-            for (let i = a; i < tabContent.length; i++) {
-                tabContent[i].classList.remove("show");
-                tabContent[i].classList.add("hide");
-            }
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove("show");
+            tabContent[i].classList.add("hide");
         }
-        hideTabContent(1); 
+    }
+    hideTabContent(1);
 
-        function showTabContent(b) {
-            if (tabContent[b].classList.contains("hide")){
-                tabContent[b].classList.remove("hide");
-                tabContent[b].classList.add("show");
-            }
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains("hide")) {
+            tabContent[b].classList.remove("hide");
+            tabContent[b].classList.add("show");
         }
+    }
 
-        info.addEventListener('click', function (event) {
-            let target = event.target;
-            if (target && target.classList.contains('info-header-tab')){
-                for (let i = 0; i < tab.length; i++) {
-                    if (target == tab[i]){
-                        hideTabContent(0);
-                        showTabContent(i);
-                        break;
-                    }
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
                 }
             }
-        
-        })
+        }
+
+    })
+
+// Timer
+
+let deadline = '2020-04-01';
+
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()), //дата конечная - дата, которая на сегодня
+    seconds = Math.floor((t/1000) % 60),
+    minutes = Math.floor((t/(1000*60) % 60)),
+    hours = Math.floor((t/(1000*60*60)  ));
+
+    return {
+        'total' : t,
+        'hours' : hours,
+        'minutes' : minutes,
+        'seconds' : seconds
+    };
+}
+
+function setClock(id, endtime) {
+    
+    let timer = document.getElementById(id), 
+    hours = timer.querySelector('.hours'),            //внутри блока с id ищет все
+    minutes = timer.querySelector('.minutes'),  
+    seconds = timer.querySelector('.seconds');
+    timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+        let t = getTimeRemaining(deadline);
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+
+        if(t.total <= 0){
+            clearInterval(timeInterval)
+        }
+    }
+
+}
+setClock('timer', deadline);
+
 
 });
